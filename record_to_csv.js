68af8baad3e72e8323c05d9b85c97d3d20fdfe50
Yustus Oktian <yustus.oktian@gmail.com>
2020-01-29 17:17:03 +0900

    add exporting to csv feature

--- record_to_csv.js
+++ record_to_csv.js
@@ -4,76 +4,78 @@ const si = require('systeminformation');
 // location to store the result
 //const RESULT_DATA_PATH = '/home/vagrant/monitoring_result.csv';
 const RESULT_DATA_PATH = 'monitoring_result.csv';
-const NUMBER_OF_CORES = 2;
 
-function consturctHeader() {
+// duration of TOTAL recording in milliseconds
+// remember to consider adding the interval duration
+// e.g. if you want to record every 1 second in 10 seconds then
+// set INTERVAL to 1000 and DURATION to 11000 (+INTERVAL)
+const DURATION = 11000;
+// duration of INTERVAL between recording in milliseonds
+const INTERVAL = 1000;
+
+// index for the network interface to be monitored
+// run 'ipconfig' or 'ifconfig'
+// count from top to bottom beginning with 0
+const NETWORK_INTERFACE_INDEX = 3;
+
+function consturctHeader(cores) {
     let string = "timestamp,mem_used,rx_bytes,tx_bytes";
-    for (i = 0; i < NUMBER_OF_CORES; i++) {
-        string += ",cpu_"+i;
+    for (i = 0; i < cores; i++) {
+        string += ",cpu_" + i;
     }
     string += "\r\n";
     return string;
 }
 
-async function main() {
-    const header = consturctHeader();
-    fs.appendFileSync(RESULT_DATA_PATH, header);
+function constructRow(time, mem, net, cpu, cores) {
+    let string = time.current + "," + mem.used + "," + net[0].rx_bytes + "," + net[0].tx_bytes;
+    for (i = 0; i < cores; i++) {
+        string += "," + cpu.cpus[i].load;
+    }
+    string += "\r\n";
+    return string;
 }
- 
-async function cpuData() {
+
+async function makeHeader() {
     try {
         const data = await si.cpu();
-        console.log('CPU Information:');
-        console.log('- manufucturer: ' + data.manufacturer);
-        console.log('- brand: ' + data.brand);
-        console.log('- speed: ' + data.speed);
-        console.log('- cores: ' + data.cores);
-        console.log('- physical cores: ' + data.physicalCores);
-        console.log('...');
+        const header = consturctHeader(data.cores);
+        fs.appendFileSync(RESULT_DATA_PATH, header);
     } catch (e) {
         console.log(e);
     }
 }
 
-async function timeData() {
+async function makeRow() {
     try {
+        const data = await si.cpu();
         const time = await si.time();
-        console.log('Current Time: ' + time.current);
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-async function cpuLoad() {
-    try {
-        const cpuLoad = await si.currentLoad();
-        console.log('Current Load: ' + cpuLoad.currentload);
-        console.log('CPU 0 Load: ' + cpuLoad.cpus[0].load);
-        console.log('CPU 1 Load: ' + cpuLoad.cpus[1].load);
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-async function ramUsage() {
-    try {
-        const ram = await si.mem();
-        console.log('Memory Total: ' + ram.total);
-        console.log('Memory Used: ' + ram.used);
-        console.log('Memory Free: ' + ram.free);
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-async function netUsage() {
-    try {
+        const mem = await si.mem();
         const netInt = await si.networkInterfaces();
-        // make sure to use the correct network interface here
-        const netStat = await si.networkStats(netInt[3].iface);
-        console.log('Network Interface: ' + netStat[0].iface);
-        console.log('Rx bytes: ' + netStat[0].rx_bytes);
-        console.log('Tx bytes: ' + netStat[0].tx_bytes);
+        const netStat = await si.networkStats(netInt[NETWORK_INTERFACE_INDEX].iface);
+        const cpuLoad = await si.currentLoad();
+
+        const row = constructRow(time, mem, netStat, cpuLoad, data.cores);
+        fs.appendFileSync(RESULT_DATA_PATH, row);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function main() {
+    try {
+        console.log('Begin Recording...');
+
+        if (fs.existsSync(RESULT_DATA_PATH)) {
+            fs.unlinkSync(RESULT_DATA_PATH);
+        }
+        makeHeader();
+
+        const timerId = setInterval(makeRow, INTERVAL);
+        setTimeout(() => {
+            clearInterval(timerId);
+            console.log('Recording Completed!');
+        }, DURATION);
     } catch (e) {
         console.log(e);
     }
